41b4b2f0f84b04319b50f4d100f11b0ca17d9521
UP2015742 <91884099+UP2015742@users.noreply.github.com>
2022-03-30 21:41:56 +0100

    updated 5

--- pract5.hs
+++ pract5.hs
@@ -11,6 +11,30 @@ rotate [] = []
 rotate[x] = [x]
 rotate (x:y:xs) = (y:x:xs)
 
-listLength :: [] -> int 
+listLength :: [a] -> Int 
 listLength [] = 0
 listLength (x:xs) = 1 + listLength (xs)
+
+multAll :: [Int] -> Int
+multAll [] = 1
+multAll (x:xs) = x * multAll xs
+
+andAll :: [Bool] -> Bool
+andAll [] = True
+andAll (True:xs) = True == andAll(xs)
+andAll (False:_) = False
+
+countElems :: Int -> [Int] -> Int
+countElems n (x:xs)
+    | listLength (x:xs) == 1 && n == x = 1
+    | listLength (x:xs) == 1 = 0
+    | n == x = 1 + countElems n (xs)
+    | n /= x = countElems n (xs)
+    |otherwise = 0
+
+removeAll :: Int -> [Int] -> [Int]
+removeAll n (x:xs)
+    | listLength (x:xs) == 1 && n == x = []
+    | listLength (x:xs) == 1 = [x]
+    | n /= x = [x] ++ removeAll n xs 
+    | n == x = removeAll n xs
