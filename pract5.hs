5196f20acd0ad5ac7a6d1d4b8d5c5085d60e1ce5
Rhys Serjent <91884099+UP2015742@users.noreply.github.com>
2022-03-31 12:51:44 +0100

    Add files via upload

--- pract5.hs
+++ pract5.hs
@@ -1,3 +1,5 @@
+type StudentMark = (String, Int)
+
 headPlusOne :: [Int] -> Int
 headPlusOne [] = 0
 headPlusOne  (x:xs) = x + 1
@@ -21,8 +23,7 @@ multAll (x:xs) = x * multAll xs
 
 andAll :: [Bool] -> Bool
 andAll [] = True
-andAll (True:xs) = True == andAll(xs)
-andAll (False:_) = False
+andAll (x:xs) = x && andAll xs
 
 countElems :: Int -> [Int] -> Int
 countElems n (x:xs)
@@ -36,5 +37,29 @@ removeAll :: Int -> [Int] -> [Int]
 removeAll n (x:xs)
     | listLength (x:xs) == 1 && n == x = []
     | listLength (x:xs) == 1 = [x]
-    | n /= x = [x] ++ removeAll n xs 
-    | n == x = removeAll n xs
+    -- | n /= x = [x] ++ removeAll n xs 
+    | n /= x = x: removeAll n xs 
+    | otherwise= removeAll n xs
+
+listMarks :: String -> [StudentMark] -> [Int]
+listMarks name stmks = [ mk | (st,mk) <- stmks , name == st ]
+
+sorted :: [Int] -> Bool
+sorted (x:y:xs)
+    | xs == [] && x <= y = True
+    | x <= y = True == sorted (y:xs)
+    |otherwise = False
+
+prefix :: [Int] -> [Int] -> Bool
+prefix [] _ = True
+prefix _ [] = False
+prefix (a:as) (b:bs) = (a==b) && prefix as bs
+    -- | a == b = prefix as bs
+    -- | otherwise = False
+
+subSequence :: [Int] -> [Int] -> Bool
+subSequence [] _ = True
+subSequence _ [] = False
+subSequence l (b:bs)
+    | prefix l (b:bs) = True
+    | otherwise = subSequence l bs
